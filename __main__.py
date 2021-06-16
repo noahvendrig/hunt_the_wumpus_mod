f6849785abeda7707a4ade6c8af9cee8258077a3
sparkz11 <noahvendrig@gmail.com>
2021-06-17 02:28:03 +1000

    e

---
+++ __main__.py
@@ -0,0 +1,84 @@
+import pygame
+import sys
+import random
+
+
+class Snake:
+    def __init__(self):
+        self.length = 1
+        self.positions = [((screen_width / 2), (screen_height / 2))]
+        self.direction = random.choice([up, down, left, right])
+        self.color = (17, 24, 47)
+        # Special thanks to YouTubers Mini - Cafetos and Knivens Beast for raising this issue!
+        # Code adjustment courtesy of YouTuber Elija de Hoog
+        self.score = 0
+
+    def get_head_position(self):
+        return self.positions[0]
+
+    def turn(self, point):
+        if self.length > 1 and (point[0] * -1, point[1] * -1) == self.direction:
+            return
+        else:
+            self.direction = point
+
+    def move(self):
+        cur = self.get_head_position()
+        x, y = self.direction
+        new = (
+            ((cur[0] + (x * gridsize)) % screen_width),
+            (cur[1] + (y * gridsize)) % screen_height,
+        )
+        if len(self.positions) > 2 and new in self.positions[2:]:
+            self.reset()
+        else:
+            self.positions.insert(0, new)
+            if len(self.positions) > self.length:
+                self.positions.pop()
+
+
+def drawGrid(surface):
+    for y in range(0, int(grid_height)):
+        for x in range(0, int(grid_width)):
+            if (x + y) % 2 == 0:
+                r = pygame.Rect((x * gridsize, y * gridsize), (gridsize, gridsize))
+                pygame.draw.rect(surface, (93, 216, 228), r)
+            else:
+                rr = pygame.Rect((x * gridsize, y * gridsize), (gridsize, gridsize))
+                pygame.draw.rect(surface, (84, 194, 205), rr)
+
+
+screen_width = 480
+screen_height = 480
+
+gridsize = 20
+grid_width = screen_width / gridsize
+grid_height = screen_height / gridsize
+
+up = (0, -1)
+down = (0, 1)
+left = (-1, 0)
+right = (1, 0)
+
+
+def main():
+    pygame.init()
+
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((screen_width, screen_height), 0, 32)
+
+    surface = pygame.Surface(screen.get_size())
+    surface = surface.convert()
+    drawGrid(surface)
+
+    while True:
+        clock.tick(10)
+
+        drawGrid(surface)
+
+        screen.blit(surface, (0, 0))
+
+        pygame.display.update()
+
+
+main()
